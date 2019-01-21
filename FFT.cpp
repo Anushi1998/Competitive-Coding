f72d591d1edf50469eb8a38a59e8eeae30ea9744
Anushi Maheshwari <anushimaheshwari95@gmail.com>
2019-01-21 20:59:59 +0530

    Update FFT.cpp
    Added Inplace FFT

--- FFT.cpp
+++ FFT.cpp
@@ -1,4 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=100010;
+const int mod=1e9+7;
 const double pi=acos(-1);
+
+int add(int x,int y){int res=(x+y)%mod;return res<0?res+mod:res;}
+int mul(int x,int y){int res=(x*1LL*y)%mod;return res<0?res+mod:res;}
+
+#define ii pair<int,int>
 
 void fft(vector<complex<double> > &a,bool inv){
     int n=a.size();
@@ -18,6 +28,33 @@ void fft(vector<complex<double> > &a,bool inv){
     }
 }
 
+//@In-place computation
+//It requires O(n) space as compared to O(nlogn) in fft, also faster :)
+//For more faster FFT (when you have strict time limits) consider taking rev array and array of complex roots of unity
+void FFT(vector<complex<double> > &a,bool inv){
+    int n=a.size(),logN=0;
+    while((1<<logN)<n) logN++;
+    auto rev=[logN](int what)->int{
+        int ans=0;
+        for(int i=0;i<logN;i++) ans|=((what>>i)&1)<<(logN-1-i);
+        return ans;
+    };
+    for(int i=0;i<n;i++) if(i>rev(i)) swap(a[i],a[rev(i)]);
+    for(int len=2;len<n;len<<=1){
+        double theta=(2*pi/len)*(inv?-1:1);
+        complex<double> wn(cos(theta),sin(theta));
+        for(int i=0;i<n;i+=len){
+            complex<double> w(1,0);
+            for(int j=i;j<i+(len<<1);j++){
+                complex<double> temp=(w*a[j+(n>>1)]);
+                a[j+(n>>1)]=a[j]-temp;a[j]=a[j]+temp;
+                w*=wn;
+            }
+        }
+    }
+    if(inv) for(auto &x:a) x.operator/=(n);
+}
+
 template<typename T,
          typename = typename enable_if<is_arithmetic<T>::value, T>::type>
 vector<T> mulPoly(vector<T> const& a,vector<T> const& b){
@@ -25,7 +62,7 @@ vector<T> mulPoly(vector<T> const& a,vector<T> const& b){
     int n=1;
     while(n<a.size()+b.size()) n<<=1;
     fa.resize(n),fb.resize(n);
-    fft(fa,0),fft(fb,0);
+    FFT(fa,0),fft(fb,0);
     for(int i=0;i<n;i++) fa[i]*=fb[i];
     fft(fa,1);
     vector<T> result(n);
@@ -37,4 +74,9 @@ vector<int> mulBigInt(vector<int> const& a,vector<int> const& b){
     int carry=0;
     for(int i=0;i<result.size();i++) result[i]+=carry,carry=(result[i]/10),result[i]%=10;
     return result;
+}
+
+
+int main(){
+    mulPoly(vector<int>(2,0),vector<int>(2,0));
 }
