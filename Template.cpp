98da3d8b0d337e669445b43922d40874ac58738f
Anushi Maheshwari <anushimaheshwari95@gmail.com>
2019-01-22 20:02:45 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=100010;
+const int mod=1e9+7;
+
+int add(int x,int y){int res=(x+y)%mod;return res<0?res+mod:res;}
+int mul(int x,int y){int res=(x*1LL*y)%mod;return res<0?res+mod:res;}
+
+#define ii pair<int,int>
+
+int main(){
+    
+    return 0;
+}
