3093bacc53b9e4709dbd0e9cb37dd8eacca4df7a
Anushi Maheshwari <anushimaheshwari95@gmail.com>
2019-01-24 16:05:59 +0530

    Create Template for UDFS

--- Data-Structures/UnionFindDisjointSet.cpp
+++ Data-Structures/UnionFindDisjointSet.cpp
@@ -1,18 +1,15 @@
-int p[N];
-int Rank[N];
-
-void UFDS(){
-    for(int i=0;i<n;i++) p[i]=i,Rank[i]=0;
-}
-
-int findSet(int x){
-    return (x==p[x])?x:p[x]=findSet(p[x]);
-}
-
-void mergeSet(int x,int y){
-    if(findSet(x)!=findSet(y)){
-        x=findSet(x),y=findSet(y);
-        if(Rank[x]>Rank[y]) p[y]=x;
-        else p[x]=y,Rank[x]+=(Rank[x]==Rank[y]);
+template<int sz> struct UF{
+  int p[sz+2],rank[sz+2],cnt[sz+2];
+  UF() { for(int i=0;i<sz;i++) p[i]=i,cnt[i]=1,rank[i]=0;}
+  void init() { for(int i=0;i<sz;i++) p[i]=i,cnt[i]=1,rank[i]=0; }
+  int operator[](int x) {return (p[x]==x)?x:p[x]=operator[](p[x]);}
+  void operator()(int x,int y) {
+    if((x=operator[](x))!=(y=operator[](y))){
+      cnt[y]=cnt[x]=cnt[x]+cnt[y];
+      if(rank[x]>rank[y]) p[x]=y;
+      else p[y]=x,rank[x]+=(rank[x]==rank[y]);
     }
-}
+  }
+};
+
+Problem 1 : https://codeforces.com/contest/1108/submission/48881492 
