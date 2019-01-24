b0ae7061ce0a732648e89feec01a0c77c4b69e5d
Anushi Maheshwari <anushimaheshwari95@gmail.com>
2019-01-24 10:54:59 +0530

    Update UnionFindDisjointSet.cpp
    rank gives error

--- Data-Structures/UnionFindDisjointSet.cpp
+++ Data-Structures/UnionFindDisjointSet.cpp
@@ -1,20 +1,18 @@
 int p[N];
-int rank[N];
+int Rank[N];
 
 void UFDS(){
-  for(int i=0;i<n;i++) p[i]=i,rank[i]=0;
+    for(int i=0;i<n;i++) p[i]=i,Rank[i]=0;
 }
 
 int findSet(int x){
-  return (x==p[x])?x:p[x]=findSet(p[x]);
+    return (x==p[x])?x:p[x]=findSet(p[x]);
 }
 
 void mergeSet(int x,int y){
-  if(findSet(x)!=findSet(y)){
-    x=findSet(x),y=findSet(y);
-    if(rank[x]>rank[y]) p[y]=x;
-    else p[x]=y,rank[x]+=(rank[x]==rank[y]);
-  }
+    if(findSet(x)!=findSet(y)){
+        x=findSet(x),y=findSet(y);
+        if(Rank[x]>Rank[y]) p[y]=x;
+        else p[x]=y,Rank[x]+=(Rank[x]==Rank[y]);
+    }
 }
-   
-Problem 1: https://codeforces.com/contest/500/submission/48393234
